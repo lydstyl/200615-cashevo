b3112dab345f81daf24856c57560a9c8b13549cd
Gabriel Brun <lydstyl@gmail.com>
2020-07-11 09:54:58 +0200

    doughnut 2

---
+++ src/components/Doughnut.vue
@@ -0,0 +1,27 @@
+<script>
+import { Doughnut, mixins } from "vue-chartjs";
+
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Doughnut,
+  mixins: [reactiveProp],
+
+  props: {
+    options: Object
+  },
+
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
+
+<style scoped>
+canvas {
+  display: block;
+  max-width: 1200px;
+  /* max-height: 600px; */
+  margin: auto;
+}
+</style>
